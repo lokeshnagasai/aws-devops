ca17a5b6d11b1aea9ef9c79a8066baf2c106f87b
LOKESH MARA <94046664+lokeshnagasai@users.noreply.github.com>
2023-05-16 22:27:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIATO4G7DZCFZAPC2OP" 
-  secret_key = "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"
+  access_key = "xxxxxxxxxxxxxxxxxxxxx" 
+  secret_key = "xxxxxxxxxxxxxxxx"
 }
 resource "aws_ssm_parameter" "foo" {
   name  = "foo"
